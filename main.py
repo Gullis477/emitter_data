f7eb254f5a8f75cda199c572e8fad41b7cb24fb7
Emil Gullbrandsson <82161482+Gullis477@users.noreply.github.com>
2025-05-05 10:20:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from emitter_data import build_emitter,Emitter,EmitterConfig, plot_signal
 if __name__ == "__main__":
     config = EmitterConfig.generate(PRI_MODULATION=3,FREQ_MODULATION=2)
     emitter = build_emitter(config)
-    signal = emitter.signal()
+    signal = emitter.signal(noise = False)
     plot_signal(signal)
     emitter.signal_to_csv("path_to_csv")
